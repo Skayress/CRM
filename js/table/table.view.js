6a4a74e34349da4b4e187cd1471060f1daa936e3
Skyress <dgroshev@axbit.ru>
2023-05-02 14:40:45 +0400

    Вывод заявок в таблице

---
+++ js/table/table.view.js
@@ -0,0 +1,29 @@
+const elements = {
+	table: document.querySelector('#tbody')
+}
+
+function renderRequest(requests) {
+	elements.table.innerHTML = '';
+
+	for (let request of requests) {
+		const template = `
+			<tr>
+				<th scope="row">${request.id}</th>
+				<td>${request.date}</td>
+				<td>${request.product}</td>
+				<td>${request.name}</td>
+				<td>${request.email}</td>
+				<td>${request.phone}</td>
+				<td>
+					<div class="badge badge-pill badge-danger">${request.status}</div>
+				</td>
+				<td>
+					<a href="edit.html">Редактировать</a>
+				</td>
+			</tr>`;
+
+		elements.table.insertAdjacentHTML('beforeend', template)
+	}
+}
+
+export { renderRequest,  elements}
